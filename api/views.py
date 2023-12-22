690f0faf7ddfe27c425207fb683c49613e49e965
cslomongo <lomongosam@gmail.com>
2023-12-22 13:29:01 +0800

    Permissions and Auth

--- api/views.py
+++ api/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 from rest_framework import generics, permissions, authentication 
 
 from .models import Items,Locations
+from api.permissions import isStaffEditor
 from api.serializers import LocationSerializer, ItemSerializer
 
 # Create your views here.
 
 class ItemList(generics.ListCreateAPIView):
     serializer_class = ItemSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [isStaffEditor]
     authentication_classes = [authentication.SessionAuthentication]
 
     def get_queryset(self):
@@ -22,14 +23,20 @@ class ItemList(generics.ListCreateAPIView):
 class ItemDetails(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ItemSerializer
     queryset = Items.objects.all()
+    permission_classes = [permissions.DjangoModelPermissions]
+    authentication_classes = [authentication.SessionAuthentication]
 
 
 class LocationList(generics.ListCreateAPIView):
     serializer_class = LocationSerializer
     queryset = Locations.objects.all()
+    permission_classes = [isStaffEditor]
+    authentication_classes = [authentication.SessionAuthentication]
 
 
 class LocationDetails(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = LocationSerializer
     queryset = Locations.objects.all()
+    permission_classes = [permissions.DjangoModelPermissions]
+    authentication_classes = [authentication.SessionAuthentication]
     
